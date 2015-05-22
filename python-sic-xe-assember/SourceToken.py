83c469a46381afa06c154bc549b2b63988260e95
loki <growingdever@gmail.com>
2015-05-22 17:55:20 +0900

    generate literals

--- python-sic-xe-assember/SourceToken.py
+++ python-sic-xe-assember/SourceToken.py
@@ -2,8 +2,10 @@ __author__ = 'loki'
 
 
 class SourceToken:
-    def __init__(self, label, operator, operands=[], generated_by_ltorg=False):
+    def __init__(self, label, operator, operands=None, generated_by_ltorg=False):
         self.label = label
         self.operator = operator
+        if operands is None:
+            operands = []
         self.operands = operands
         self.generated_by_ltorg = generated_by_ltorg
--- python-sic-xe-assember/main.py
+++ python-sic-xe-assember/main.py
@@ -50,7 +50,7 @@ def parsing_token(line):
     if len(tokens) == 1:
         source_tokens.append(SourceToken('', tokens[0]))
     elif len(tokens) == 2:
-        if tokens[0] == 'CSECT':
+        if tokens[1] == 'CSECT':
             source_tokens.append(SourceToken(tokens[0], tokens[1]))
         else:
             source_tokens.append(
@@ -71,7 +71,64 @@ def parsing_all_tokens():
 
 
 def generate_literals():
-    pass
+    literals = []
+    generate_target_position = []
+
+    control_section_num = 0
+    num_of_ltorg = 0
+    for i in xrange(0, len(source_tokens)):
+        token = source_tokens[i]
+        if token.operator == 'START' or token.operator == 'CSECT':
+            control_section_num += 1
+            generate_target_position.append((i, False))
+            continue
+
+        if token.operator == 'LTORG':
+            generate_target_position.append((i - num_of_ltorg, True))
+            continue
+
+        if len(token.operands) == 0:
+            continue
+
+        operand1 = token.operands[0]
+        if operand1[0] == '=':
+            is_found = False
+            for tp in literals:
+                if tp[0] == operand1:
+                    is_found = True
+            if not is_found:
+                literals.append((operand1, i))
+
+    generate_target_position.append((len(source_tokens) - 1, False))
+
+    generated = 0
+    last = 0
+    for target_index in generate_target_position:
+        new_tokens = []
+        for i in xrange(last, len(literals)):
+            pair = literals[i]
+            if pair[1] < target_index[0]:
+                literal = pair[0]
+
+                if literal[1] == 'X' or literal[1] == 'C':
+                    new_token = SourceToken('', 'BYTE')
+                else:
+                    new_token = SourceToken('', 'WORD')
+
+                new_token.label = literal
+                new_token.generated_by_ltorg = target_index[1]
+                new_token.operands.append(literal[1:])
+                new_tokens.append(new_token)
+
+                generated += 1
+                last = i + 1
+
+        index = target_index[0] + generated - len(new_tokens)
+        source_tokens[index:index] = new_tokens
+
+    for token in source_tokens:
+        if token.operator == 'LTORG':
+            source_tokens.remove(token)
 
 
 def add_all_symbols():
