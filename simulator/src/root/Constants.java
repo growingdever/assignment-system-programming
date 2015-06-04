a73118550681f7598abff5a2da2a4c3540349923
loki <growingdever@gmail.com>
2015-06-04 11:58:48 +0900

    JSUB format3 부분 고침.

--- simulator/src/root/Constants.java
+++ simulator/src/root/Constants.java
@@ -7,6 +7,10 @@ public class Constants {
     public static final int REGISTER_A = 0;
     public static final int REGISTER_X = 1;
     public static final int REGISTER_L = 2;
+    public static final int REGISTER_B = 3;
+    public static final int REGISTER_S = 4;
+    public static final int REGISTER_T = 5;
+    public static final int REGISTER_F = 6;
     public static final int REGISTER_PC = 8;
     public static final int REGISTER_SW = 9;
 }
--- simulator/src/root/instruction/InstJSUB.java
+++ simulator/src/root/instruction/InstJSUB.java
@@ -17,18 +17,29 @@ public class InstJSUB extends SICXEInstruction {
         int currPC = virtualMachine.getRegister(Constants.REGISTER_PC);
         virtualMachine.setRegister(Constants.REGISTER_L, currPC);
 
-        int address = 0;
         if(isExtended) {
+            int address = 0;
             address += (bytes[1] & 0xF) << 16;
             address += bytes[2] << 8;
             address += bytes[3];
 
             virtualMachine.setRegister(Constants.REGISTER_PC, address);
         } else {
-            address += (bytes[1] & 0xF) << 16;
-            address += bytes[2] << 8;
-            address += bytes[3];
+            boolean isRelativePC = (bytes[1] & 0x20) > 0;
+            boolean isRelativeBase = (bytes[1] & 0x40) > 0;
 
+            int diff = 0;
+            diff += (bytes[1] & 0xF) << 8;
+            diff += (bytes[2] & 0xFF);
+
+            int regNum = Constants.REGISTER_PC;
+            if( isRelativePC ) {
+                regNum = Constants.REGISTER_PC;
+            } else if( isRelativeBase ) {
+                regNum = Constants.REGISTER_B;
+            }
+
+            int address = virtualMachine.getRegister(regNum) + diff;
             virtualMachine.setRegister(Constants.REGISTER_PC, address);
         }
     }
